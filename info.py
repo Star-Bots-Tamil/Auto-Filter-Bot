1b4b48e2bcf32cc563b1a7d9e886dff47e970657
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-11 19:37:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://web-2wezrgird60h.up-de-fra1-k8s-1.apps.run-on-seenode.com/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
