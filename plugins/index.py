88b77e98661c90a6e8e5fd7ae3da0fe539a5b1db
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-12 00:09:28 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -27,7 +27,7 @@ async def index_files(bot, query):
         await query.message.edit("Trying to cancel Indexing...")
 
 
-@Client.on_message(filters.private & filters.incoming & filters.user(ADMINS))
+@Client.on_message(filters.private & filters.forwarded & filters.incoming & filters.user(ADMINS))
 async def send_for_index(bot, message):
     if lock.locked():
         return await message.reply("Wait until the previous process completes.")
