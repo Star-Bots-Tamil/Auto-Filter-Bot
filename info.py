5c0a46b085653b2a92805a19f4f81a2db2906b19
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-12 08:29:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ else:
     ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002267578638').split(" ")]
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002267578638 -1001816164988').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1001821439025')
