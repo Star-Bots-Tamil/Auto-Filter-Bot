8092f2536378879d6f1461db085858978e6d96f0
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2025-01-20 22:43:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1006,3 +1006,11 @@ async def vsong(client, message: Message):
         if files and os.path.exists(files):
             os.remove(files)
             
+@Client.on_message(filters.command("stickerid") & filters.private)
+async def stickerid(bot, message):
+    s_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Sticker")
+    if s_msg.sticker:
+        await s_msg.reply_text(f"**Sticker ID is**  \n `{s_msg.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{s_msg.sticker.file_unique_id}`")
+    else: 
+        await s_msg.reply_text("Oops !! Not a sticker file")
+        
